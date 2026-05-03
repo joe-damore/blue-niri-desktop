fa8d5d936456b4128cfe00dbd3a6c731a9ed8d0e
Joe D'Amore <joe@joedamore.me>
2026-05-03 12:24:21 -0400

    Install basic packages, set basic configuration

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,14 +3,35 @@
 set -ouex pipefail
 
 ### Install packages
+# Fonts
+dnf5 install -y rsms-inter-fonts fira-code-fonts
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# Icons, themes, etc.
+dnf5 -y install papirus-icon-theme
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Dev Tools
+dnf5 -y install @virtualization
+dnf5 -y install helix
+dnf5 -y install podman-compose container-selinux
+dnf5 -y install kitty
+
+# System Utilities
+dnf5 -y install gparted bluez fastfetch zsh
+
+# Install Sublime HQ GPG key to facilitate install of Sublime Text, Sublime Merge.
+# Enable Sublime Text and Sublime Merge repo, but do not install either by default.
+wget https://download.sublimetext.com/sublimehq-rpm-pub.gpg -O /etc/pki/rpm-gpg
+dnf5 -y config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
+# Enable Tailscale GPG key to facilitate install of Tailscale, but do not install Tailscale by default.
+dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+# Enable RPMFusion
+dnf5 -y install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
 
 # Use a COPR Example:
 #
@@ -20,5 +41,6 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
+systemctl enable libvirtd
+systemctl enable bluetooth
